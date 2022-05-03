ed97765c055187aad9c20bfecd78d4639972d5ec
kovacsbalazspeter21 <71692659+kovacsbalazspeter21@users.noreply.github.com>
2022-05-03 14:19:36 +0200

    Add files via upload

---
+++ acbbasketball.py
@@ -0,0 +1,53 @@
+'''
+hazai	idegen	hazai_pont	idegen_pont	helysz�n	id�pont
+7up Joventut	Adecco Estudiantes	81	73	Palacio Mun. De Deportes De Badalona	2005-04-03
+'''
+
+class Project():
+    def __init__(self, sor):
+        hazai, idegen, hazai_pont, idegen_pont, helyszin, idopont = sor.strip().split(";")
+        self.hazai = hazai
+        self.idegen = idegen
+        self.hazai_pont = hazai_pont
+        self.idegen_pont = idegen_pont
+        self.helyszin = helyszin
+        self.idopont = idopont
+        
+with open("eredmenyek.txt", "r", encoding="latin2") as f:
+    cim_sor = f.readline()
+    lista = [Project(sor) for sor in f]
+
+
+#3
+madridh = [sor for sor in lista if sor.hazai == "Real Madrid"]
+madridi = [sor for sor in lista if sor.idegen == "Real Madrid"]
+print(f"3. Feladat: Real Madrid: Hazai: {len(madridh)}, Idegen: {len(madridi)}")        
+
+#4
+
+dontettlen = [sor for sor in lista if sor.hazai_pont == sor.idegen_pont]
+ha_nem_dontettlen = [sor for sor in lista if sor.hazai_pont != sor.idegen_pont]
+if len(dontettlen) != 0:
+    print("4. Feladat: Volt döntettlen? Igen")
+else:
+    print("4. Feladat: Volt döntettlen? Nem")
+    
+#5
+barca = [sor.hazai for sor in lista if "Barcelona" in sor.hazai][0]
+
+print(f"5. Feladat: barcelonai csapat neve: {barca}")
+
+
+
+#6
+
+meccsek = [sor for sor in lista if sor.idopont == "2004-11-21"]
+[print(f"         {sor.hazai} - {sor.idegen} ({sor.hazai_pont}:{sor.idegen_pont})") for sor in meccsek]
+
+
+#7
+'''
+print("7.Feladat: Statisztika")
+stat = [sor for sor in lista stat[sor.helyszin] = stat.get(sor.ev, 0) + 1]
+[for kulcs, ertek in stat.items() print(f"       {kulcs} - {ertek} ország")]
+'''    
